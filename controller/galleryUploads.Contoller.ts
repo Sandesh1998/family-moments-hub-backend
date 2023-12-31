69f8c3c1826bbdecd0be80eff59fb89dbcfdbd53
Sandesh1998 <sandeshpoudel144@gmail.com>
2023-12-31 18:02:29 +0545

    feat : upload gallery

--- controller/galleryUploads.Contoller.ts
+++ controller/galleryUploads.Contoller.ts
@@ -17,8 +17,13 @@ export const getPhotos = async (req: Request, res: Response) => {
 export const uploadPhotos = async (req: Request, res: Response): Promise<void> => {
     try {
         const files = req.files as Express.Multer.File[];
+        if (!files) {
+            res.status(400).json({ error: 'No files provided' });
+            return;
+        }
+
         const photos: IPhoto[] = [];
-    
+
         files.forEach((file) => {
             const { path, mimetype } = file;
             const photo = new Photo({
@@ -27,14 +32,14 @@ export const uploadPhotos = async (req: Request, res: Response): Promise<void> =
             });
             photos.push(photo);
         });
-    
+
         await Photo.insertMany(photos);
         res.json({ message: "Photos uploaded successfully" });
     } catch (error) {
       console.error(error);
       res.status(500).json({ error: 'Internal Server Error' });
     }
-  };
+};
 
 
   
